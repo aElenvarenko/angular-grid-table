50c09685d12f7ae28f63f90d68d4233fed6b355a
Alex Elenvarenko <a.elenvarenko@estp.ru>
2015-05-08 12:33:12 +0300

    Add filter width

--- src/js/directives/grid-table-header.js
+++ src/js/directives/grid-table-header.js
@@ -9,15 +9,6 @@ grid.directive('gridTableHeader', [
 			require: '^gridTable',
 			templateUrl: function () {
 				return cGlobals.tplUrl + 'grid-table-header.html';
-			},
-			link: function (scope, element, attrs, ctrl) {
-				var activeFilter;
-				element.on('focusin', '.grid-table-filter input, .grid-table-filter select', function () {
-					activeFilter = $(this).attr('id');
-				});
-				ctrl.set('addEvent', 'onFilter', function () {
-					element.find('.grid-table-filter #' + activeFilter).focus();
-				});
 			}
 		};
 	}
